115bda9069e9388622c982aaf2e291167b21af36
Alex Menendez <amenendez15@usf.edu>
2025-10-01 13:51:38 -0400

    steps 1-4 done

--- cc_6.js
+++ cc_6.js
@@ -1,10 +1,37 @@
-//Step 1
+//Step 2
 class Employee{
     constructor(name, department){
     this.name = name;
     this.department= department;
     }
     describe(){
-        return `Hello ${this.name}!`;
+        return `Hello ${this.name}! You look dashing today!`;
     }
 }
+//Step 3
+
+class Manager extends Employee {
+    constructor(name, department, teamSize)
+    {
+        super(name,department);
+        this.teamSize= teamSize
+    }
+   describe()
+   {
+        return `Hello ${this.name}! You manage a team of ${this.teamSize} in the ${this.department} department!`;
+    }
+    
+}
+
+//Step 4
+
+const emp1= new Employee ("Itadori Yuji", "Accounting");
+console.log(emp1.describe())
+
+const emp2= new Employee ("Megumi Fushiguro", "Accounting");
+console.log(emp2.describe())
+
+const emp3 =new Manager ("Satoru Gojo", "Accounting", 4);
+console.log(emp3.describe())
+
+//Step 5
